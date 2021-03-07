d6cf567999c3f12b7e2557e617a0a9f0fdcd80ff
Kiko Estrada <uldarico@outlook.com>
2021-03-06 20:17:57 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `jonqjbn2efkl`,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
     {
